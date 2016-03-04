28eefd0b398f75b79a5acb40fde6f518ad7dbdc5
Jay Cheroske <jay@portal.org>
2016-03-03 22:56:10 -0700

    Initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'jcheroske:modernizr-touch',
+  version: '3.3.1',
+  summary: 'Modernizr with just the touch events test.',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('index.js', 'client');
+});
